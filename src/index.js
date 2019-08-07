257fac230277f535f4c995335834003ae00c1140
Gabriel Mijares <gabrielmljob@gmail.com>
2019-08-08 00:54:09 +0200

    add index.js and index.css

---
+++ src/index.js
@@ -0,0 +1,46 @@
+//@ts-check
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+	renderSquare() {
+		return (
+			<button className="square">
+				{/* TODO */}
+			</button>
+		);
+	}
+}
+
+class Board extends React.Component {
+	renderSquare(i) {
+		return <Square />;
+	}
+
+	render() {
+		const status = 'Next player: X';
+
+		return (
+			<div>
+				<div className="status">{status}</div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="boar-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		);
+	}
+}
