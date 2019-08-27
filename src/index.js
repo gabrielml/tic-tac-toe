745da9fa2b2440b7edeb3a0085ef5ea1a7da616f
Gabriel Mijares <gabrielmljob@gmail.com>
2019-08-28 00:21:40 +0200

    add Game component and ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-	renderSquare() {
+	render() {
 		return (
 			<button className="square">
 				{/* TODO */}
@@ -44,3 +44,26 @@ class Board extends React.Component {
 		);
 	}
 }
+
+class Game extends React.Component {
+	render() {
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board />
+				</div>
+				<div className="game-info">
+					<div>{/* status */}</div>
+					<ol>{/* TODO */}</ol>
+				</div>
+			</div>
+		);
+	}
+}
+
+// =======================================
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById('root')
+);
